ca88fc3cd63da68458fd0fd3cfd0753bb0ae8013
pramod <pramod.r03@infosys.com>
2024-05-13 09:46:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import MainPage from './Components/MainPage';
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainPage.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './Mainpage.css'
+function MainPage() {
+    return ( <div className="mainpage">
+        <h1>Welcome to My Website</h1>
+        <button className='button'>Click Here</button>
+    </div>);
+}
+
+export default MainPage;
